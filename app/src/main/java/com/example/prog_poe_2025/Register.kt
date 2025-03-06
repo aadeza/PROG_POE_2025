a73adbbe27ab2bec0499703920931ff96a0f929e
aadeza <D3za.silongo@gmail.com>
2025-03-06 11:48:49 +0200

    Updated register-layout

--- app/src/main/java/com/example/prog_poe_2025/Register.kt
+++ app/src/main/java/com/example/prog_poe_2025/Register.kt
@@ -2,28 +2,26 @@ package com.example.prog_poe_2025
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-
-        //Link to register
-        val registerText = findViewById<TextView>(R.id.txtToRegister)
-        registerText.setOnClickListener {
-            val intent = Intent(this, Register::class.java)
+        //Link Back to Login
+        val DoneReg = findViewById<Button>(R.id.btnDoneReg)
+        DoneReg.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
