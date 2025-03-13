f0cc10a7fa8307d60f9896ed360b7e9a5cf9b3dc
aadeza <D3za.silongo@gmail.com>
2025-03-13 12:23:09 +0200

    Log income / expenses layout

--- app/src/main/java/com/example/prog_poe_2025/Home.kt
+++ app/src/main/java/com/example/prog_poe_2025/Home.kt
@@ -1,21 +1,35 @@
 package com.example.prog_poe_2025
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
+
         }
+
+        //Link to Create Budget
+        val createbudget = findViewById<Button>(R.id.btnCreateBudget)
+        createbudget.setOnClickListener(){
+            val intent = Intent(this, CreateBudget::class.java)
+            startActivity(intent)
+        }
+
+        //link to Log Income Expense
 
     }
 }
