8019c6133109522212ae6763091f80673c0746f5
Reaobaka <reaobakan@icloud.com>
2025-04-27 21:26:37 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/prog_poe_2025/HomeRepository.kt
@@ -0,0 +1,23 @@
+package com.example.prog_poe_2025
+
+import DAOs.ExpensesDAO
+import DAOs.IncomeDAO
+import Data_Classes.Expenses
+import Data_Classes.Income
+
+
+class HomeRepository(
+    private val incomeDao: IncomeDAO,
+    private val expensesDao: ExpensesDAO
+) {
+
+    suspend fun getLatestIncomes(limit: Int): List<Income> {
+        return incomeDao.getLatestIncomes(limit)
+    }
+
+    suspend fun getLatestExpenses(limit: Int): List<Expenses> {
+        return expensesDao.getLatestExpense(limit)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/prog_poe_2025/HomeViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.prog_poe_2025
+
+class HomeViewModel {
+}
